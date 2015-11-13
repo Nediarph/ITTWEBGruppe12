46c22aa6bdb26fa52bc5cc4bd344dc40d4119963
Claus <201271020@iha.dk>
2015-11-13 20:11:15 +0100

    Added View with table. Much work left on it

---
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/Controllers/TableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Opligatorisk_opgave_1.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult ComponentTable()
+        {
+            return View();
+        }
+    }
+}
---
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/Models/TableViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Opligatorisk_opgave_1.Models
+{
+    public class ComponentTable
+    {
+        
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
