53f9f1d88f6a9f197ee8d56be4bc55f9e2126133
Claus <201271020@iha.dk>
2015-11-17 23:25:55 +0100

    Added Index View and worked with login and tabel

--- Opligatorisk opgave 1/Opligatorisk opgave 1/Controllers/TableController.cs
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/Controllers/TableController.cs
@@ -8,8 +8,17 @@ namespace Opligatorisk_opgave_1.Controllers
 {
     public class TableController : Controller
     {
+        //private ScrumCatDataEntities db = new ScrumCatDataEntities();
         // GET: Table
         public ActionResult ComponentTable()
+        {
+            //var tmp = db.Invites.ToList();
+            var toView = new List<String> {};
+
+            return View(toView);
+        }
+
+        public ActionResult Index()
         {
             return View();
         }
