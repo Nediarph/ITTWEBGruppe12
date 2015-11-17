bb8a0c333a0acb9906b6cc3c51ab8ee6bf08e033
Jonas Thirup Larsen <nediarph@gmail.com>
2015-11-17 23:30:40 +0100

    Finished DB stuff, added POCOs for DB access. Also made the repository interfaces.

---
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/DAL/IReadEmbeddedStockRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opligatorisk_opgave_1.DAL
+{
+    public interface IReadEmbeddedStockRepository
+    {
+        IEnumerable<Components> GetAllComponents();
+        IEnumerable<SpecificComponents> GetAllSpecificComponentsById(int id);
+        LoanInformation GetLoanInformationBySpecificId(int specId);
+        IEnumerable<Categories> GetAllCategories();
+
+    }
+}
---
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/DAL/IWriteEmbeddedStockRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opligatorisk_opgave_1.DAL
+{
+    public interface IWriteEmbeddedStockRepository
+    {
+        void AddNewCategory(Categories category);
+        void RemoveCategory(int catId);
+        void AddNewComponent(Components comp);
+        void AddNewSpecificComponent(SpecificComponents specComp);
+        void RemoveComponent(int compId);
+        void RemoveSpecificComponent(int specCompId);
+        void AddLoanInformation(LoanInformation loan);
+        void RemoveLoanInformation(int loanId);
+    }
+}
