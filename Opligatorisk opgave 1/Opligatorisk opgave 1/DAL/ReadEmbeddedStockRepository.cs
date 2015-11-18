7c9598233e4b34e30217225bb3c48991521e494a
Jonas Thirup Larsen <nediarph@gmail.com>
2015-11-18 13:40:04 +0100

    Start of Repository implementation.

---
+++ Opligatorisk opgave 1/Opligatorisk opgave 1/DAL/ReadEmbeddedStockRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+namespace Opligatorisk_opgave_1.DAL
+{
+    public class ReadEmbeddedStockRepository : IReadEmbeddedStockRepository
+    {
+        public ReadEmbeddedStockRepository()
+        {
+            
+        }
+        public IEnumerable<Components> GetAllComponents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<SpecificComponents> GetAllSpecificComponentsById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LoanInformation GetLoanInformationBySpecificId(int specId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Categories> GetAllCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
