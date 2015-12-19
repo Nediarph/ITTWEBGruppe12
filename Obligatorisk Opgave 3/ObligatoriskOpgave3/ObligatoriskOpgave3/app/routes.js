323c8f7fb22a3eb5b6c02c68b9d5c8fd3464bbad
Claus <201271020@iha.dk>
2015-12-19 22:05:18 +0100

    Added routes for workout plans

--- Obligatorisk Opgave 3/ObligatoriskOpgave3/ObligatoriskOpgave3/app/routes.js
+++ Obligatorisk Opgave 3/ObligatoriskOpgave3/ObligatoriskOpgave3/app/routes.js
@@ -1,8 +1,10 @@
-﻿var Person = require('/models/users');
+﻿var Person = require('./models/users');
+var Workout = require('./models/workOutPlan');
 
 module.exports = function (app) {
     
-//Backend routes ====================
+    //Backend routes ====================
+    //User routes ====================
     app.route('/users')
         .get(function(req, res) {
             Person.find(function(err, users) {
@@ -67,8 +69,83 @@ module.exports = function (app) {
             });
         });
 
-    
+    //WorkOutPlan routes ====================
+    app.route('/workoutplan/')
+        .get(function(req, res) {
+            Workout.find(function(err, workOutPlans) {
+                if (err)
+                    res.send(err);
 
+                res.json(workOutPlans);
+            });
+        })
+        .post(function (req, res) {
+
+            var workout = new Workout();
+            workout.name = req.body.name;
+            workout.userEmail = req.body.userEmail;
+            workout.completeCount = req.body.completeCount;
+            workout.lastCompleteDate = req.body.lastCompleteDate;
+
+            workout.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Workout plan created' });
+            });
+        });
+    app.route('/workoutplan/:plan_id')
+        .put(function (req, res) {
+        
+        Workout.findById(req.params.plan_id, function (err, plan) {
+            if (err)
+                res.send(err);
+            
+            plan.name = req.body.name;
+            plan.completeCount = req.body.completeCount;
+            plan.lastCompleteDate = req.body.lastCompleteDate;
+            
+            plan.save(function (err) {
+                if (err)
+                    res.send(err);
+                
+                res.json({ message: 'Workout plan updated' });
+            });
+        });
+
+    })
+        .delete(function (req, res) {
+        Workout.remove({
+            _id: req.params.plan_id
+        }, function (err, plan) {
+            if (err)
+                res.send(err);
+            
+            res.json({ message: 'Successfully deleted Workout plan' });
+        });
+    });
+    app.route('/addexercise/:plan_id')
+        .put(function(req, res) {
+
+            Workout.findById(req.params.plan_id, function(err, plan) {
+                if (err)
+                res.send(err);
+
+                plan.exercises.push({
+                    name: req.body.name,
+                    describtion: req.body.describtion,
+                    exerciseset: req.body.exerciseset,
+                    repititions: req.body.repititions
+                });
+
+                plan.save(function(err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Exercise added' });
+                });
+            });
+        });
 
 //frontend routes ===================
     app.get('*', function(req, res) {
